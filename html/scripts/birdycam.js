ba579d8d191535db8c92eefd599deb17dd884cca
Belin Fieldson <thebelin@gmail.com>
2016-05-26 04:57:53 -0600

    Add a chat module

---
+++ html/scripts/birdycam.js
@@ -0,0 +1,149 @@
+// BirdyCam client object
+// Object Literal Pattern
+var _BirdyCam = {
+  // To start things, run this when jQuery is ready
+  init: function () {
+    // preload all the jQuery components using native document id method
+    var $dom = {
+        playerImage:  $(document.getElementById('playerImage')),
+        fullscreen:   $(document.getElementById('btnFullscreen')),
+        player:       $(document.getElementById('vidPlayer')),
+        userCount:    $(document.getElementById('userCount')),
+        playerStatus: $(document.getElementById('playerStatus')),
+        statusIcon:   $(document.getElementById('statusIcon'))
+      },
+
+    // For getting the user count
+      getUserCount = function () {
+        $.get('/usercount', function (userData) {
+          if (userData.count) $dom.userCount.html(userData.count);
+        });
+      };
+
+    console.log('init BirdyCam', self);
+
+    // Get user count initially
+    getUserCount();
+
+    // monitor for new users and adjust the counter accordingly
+    window.setInterval(getUserCount, 5000);
+
+    console.log('fullscreen:', $dom.fullscreen);
+    // make the controls work
+    $dom.fullscreen.on('click', function (eventObj) {
+      if ($dom.player.hasClass('minPlayer')) {
+        $dom.player.addClass('fullScreen').removeClass('minPlayer');
+      } else {
+        $dom.player.addClass('minPlayer').removeClass('fullScreen');
+      }
+    });
+
+    // Show the status of the video playback to the user
+    $dom.playerImage.ready(function (eventObj) {
+      $dom.playerStatus.html('playing');
+    });
+  }
+};
+
+// BirdyChat client object
+var _BirdyChat = {
+  // A function which is used to start up the BirdyChat object
+  init: function () {
+    // A socket.io reference
+    var socket = io(),
+
+    // The dom objects to be used in this object
+      $dom = {
+        messageBox: $(document.getElementById('m')),
+        messages:   $(document.getElementById('messages')),
+        nickname:   $(document.getElementById('nickname')),
+        formObj:    $('form'),
+        uiMessage:  $(document.getElementById('uiMessage'))
+      },
+
+    // a bit tracking the state of the preload
+      preloaded = false,
+
+    // scrolls to the bottom of the messages
+      updateScroll = function () {
+        var element = document.getElementById("messages");
+        element.scrollTop = element.scrollHeight;
+      },
+
+    // A message
+      showMessage = function (msg) {
+        $dom.messages
+          .append($('<span>').text(msg.nickname).addClass('nickname'))
+          .append($('<span>').text(msg.message).addClass('userMessage'));
+        updateScroll();
+      },
+
+    // a ui message
+      showUiMessage = function (message) {
+        $dom.uiMessage.text(message).show();
+        setTimeout(function () {
+          $dom.uiMessage.hide();
+        }, 1300)
+      },
+
+    // Save the user nickname
+      saveNick = function (nickname) {
+        window.localStorage.setItem('nickname', nickname);
+      },
+
+      getNick = function () {
+        return window.localStorage.getItem('nickname');
+      };
+
+    // Set the nickname to whatever was last used
+    $dom.nickname.val(getNick());
+
+    // activate the form submit routine
+    $dom.formObj.submit(function () {
+      // Create a message based on the form contents
+      var chatMessage = {
+        nickname: $dom.nickname.val(),
+        message: $dom.messageBox.val()
+      };
+
+      // Don't allow blank messages or nicknames
+      if (!chatMessage.nickname) {
+        showUiMessage('Specify a nickname to send messages.');
+        return false;
+      }
+      if (!chatMessage.message) {
+        showUiMessage('Specify a message to send.');
+        return false;
+      }
+
+      // Store the nickname
+      saveNick(chatMessage.nickname);
+      console.log('sending chat message: ', chatMessage);
+
+      socket.emit('chat message', chatMessage);
+      $dom.messageBox.val('');
+      return false;
+    });
+
+    // socket.io connectors to make messaging work
+    socket.on('chat message', function (msg) {  
+      showMessage(msg);
+    });
+
+    socket.on('chat connection', function (messages) {
+      if (!preloaded) {
+        preloaded = true;
+        messages.forEach(function (msg) {
+          showMessage(msg);
+        });
+      }
+    });
+    
+  }
+};
+
+// Init the literals when the page is ready
+$(document).ready(function () {
+  _BirdyCam.init();
+  _BirdyChat.init();
+});
--- index.js
+++ index.js
@@ -8,7 +8,30 @@ var MjpegProxy = require('mjpeg-proxy').MjpegProxy,
   app = express(),
 
 // An http server
-  server = require('http').createServer(app);
+  server = require('http').createServer(app),
+
+// Socket.io for chats
+  io = require('socket.io')(server),
+
+// the body-parser for form processing
+  bodyParser = require('body-parser'),
+
+// Chat Data
+  chatData = require(__dirname + '/chatdata.json'),
+
+// Chat model
+  Chat = function (nickname, message) {
+    // prevent script injection
+    this.nickname = typeof nickname === 'string' ? nickname.replace(/<script.*?>.*?<\/script>/igm, '') : '';
+    this.message = typeof message === 'string' ? message.replace(/<script.*?>.*?<\/script>/igm, '') : '';
+    this.entryDate = Date.now();
+  };
+
+// Make the POST and PUT data available
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Root path for serving support files from site
 console.log('starting static http server on html files');
@@ -21,8 +44,37 @@ app.get('/usercount', function (req, res) {
   });
 });
 
-// @todo build a chat room on the feed
+// socket.io based chat system
+io.on('connection', function (socket) {
+  // chat user connected
+  console.log('a chat user connected', chatData);
 
-app.get('/index1.jpg', new MjpegProxy('http://24.9.117.243/html/cam_pic_new.php?time=1464187543304&pDelay=40000').proxyRequest);
+  // Get the current chatData, last 100 entries max
+  io.emit('chat connection', chatData);
+
+  socket.on('chat message', function (msg) {
+    // make sure that message doesn't have scripts injected
+    msg = new Chat(msg.nickname, msg.message)
+    console.log('msg: ', msg);
+    // post a chat entry
+    chatData.push(msg);
+    require('fs').writeFileSync(__dirname + '/chatdata.json', JSON.stringify(chatData));
+    console.log(chatData);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
+
+// Mjpeg Feed
+app.get(
+  '/index1.jpg',
+  new MjpegProxy('http://24.9.117.243/html/cam_pic_new.php?time=1464187543304&pDelay=40000'
+).proxyRequest);
+
+// Start the server
 server.listen(8080);
 console.log('listening on 8080');
